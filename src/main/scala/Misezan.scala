9d7b2e349201c741eb9a94a534268b12df9ef79d
bizyutyu <bizyuness.takakura@gmail.com>
2024-03-31 22:17:06 +0900

    add: FlatSpecスタイルを採用し、テストを追加

--- src/main/scala/Misezan.scala
+++ src/main/scala/Misezan.scala
@@ -1,5 +1,13 @@
 class Misezan(){
+    
+    def isEqual(num1: Int, num2: Int): Boolean = {
+        num1 == num2
+    }
+
     def calculate(arg1: Int, arg2: Int): Int = {
-        arg1 + arg2
+        (arg1, arg2) match {
+            case (a, b) if isEqual(a, b) => 0
+            case _ => arg1+arg2
+        }
     }
 }
